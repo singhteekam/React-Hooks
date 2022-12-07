472202ee5a5dd29a94d5f42c5ebc6fef7432b915
Teekam Singh <teekams10999@gmail.com>
2022-12-07 17:14:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
             <Route index element={<Homepage />} />
-            <Route path='/usestate' element={<UseStatePage />} />
+            <Route path='/' element={<UseStatePage />} />
         </Routes>
       </BrowserRouter>
     </>
