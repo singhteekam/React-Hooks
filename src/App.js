1af66ccd1ccc3bc73022dd06b4885dbc91712a0e
Teekam Singh <teekams10999@gmail.com>
2022-12-07 23:56:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Navigation from './pages/Navigation';
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
             <Route path='/' element={<Homepage />} />
             <Route path='/usestate' element={<UseStatePage />} />
